f27bb9437d4c9b592975f8b06bb016db0abe4e60
하수종 <hsz3855@gmail.com>
2023-11-17 23:23:25 +0900

    add : 프로젝트에서 사용할 컴포넌트를 생성한다

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import Controller from './component/Controller';
+import Viewer from './component/Viewer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section> {/* section은 영역 분리를 위한 태그임. div와 동일한 기능을 수행함  */}
+        <Viewer/>
+      </section>
+      <section>
+        <Controller/>
+      </section>
+    </div>
+  )
 }
 export default App;
