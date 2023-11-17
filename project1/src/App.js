202059dcbd264838c1273dd713b0b3952b83e269
하수종 <hsz3855@gmail.com>
2023-11-17 23:43:55 +0900

    add : 카운터 기능 구현하기 - 1

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section> {/* section은 영역 분리를 위한 태그임. div와 동일한 기능을 수행함  */}
-        <Viewer/>
+        <Viewer count={count}/>
       </section>
       <section>
-        <Controller/>
+        <Controller handleSetCount={handleSetCount}/>
       </section>
     </div>
   )
