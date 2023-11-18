24730858387a8cc4035bd9ce29f82761fe43417a
하수종 <hsz3855@gmail.com>
2023-11-18 16:05:55 +0900

    add : useEffect를 사용한 변경 감지

---
+++ project1/README.md
@@ -0,0 +1,14 @@
+# Project1
+_chapter6과 연계해서 진행함_
+
+## 리액트 컴포넌트 라이프 사이클
+
+Mount -> Update -> Unmount
+
+각 과정의 의미는 아래와 같다.
+
+`Mount`는 화면에 랜딩 되어 있는 상태
+
+`Update`는 부모가 재랜더링 되거나, Props나 State를 통해서 값이 바뀐 상태
+
+`Unmount`는 화면에서 컴포넌트가 제거 된 상태
--- project1/src/App.js
+++ project1/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
@@ -6,13 +6,30 @@ import Viewer from './component/Viewer';
 function App() {
 
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
 
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  }
+
+  useEffect(() => { // 첫번쨰 인수로 콜백 함수, 두번쨰 인수로 의존성 배열
+    console.log("count 업데이트 : ", count);
+  }, [count]); // useEffect는 의존성 배열이 변경되면 콜백 함수를 실행하도록 동작함.
+
+  useEffect(() => {
+    console.log("업데이트 : ", text, count);
+  }, [count, text]);
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText}></input>
+      </section>
       <section> {/* section은 영역 분리를 위한 태그임. div와 동일한 기능을 수행함  */}
         <Viewer count={count}/>
       </section>
