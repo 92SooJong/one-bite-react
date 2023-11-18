ee18ef53914599428c9dc997e75ae95320cd3b71
하수종 <hsz3855@gmail.com>
2023-11-18 21:51:53 +0900

    add : TodoList UI 만들기

---
+++ project2/src/component/TodoList.js
@@ -0,0 +1,14 @@
+import "./TodoList.css"
+
+
+const TodoList = () => {
+    return (
+    <div className="TodoList">
+        <h4>Todo List 🌱</h4>
+        <input className="searchbar" placeholder="검색어를 입력하세요"></input>
+    </div>
+    
+    )
+};
+
+export default TodoList;
