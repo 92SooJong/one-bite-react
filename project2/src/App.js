0f77515c54129b2f1914a0cca7635fcc94ac5660
Joey <hsz3855@gmail.com>
2023-11-26 01:25:22 +0900

    add : 할일 CRUD를 useReducer를 사용하도록 수정

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { useRef, useReducer } from "react"
 import './App.css';
 import Header from './component/Header';
 import TestComp from "./component/TestComp";
@@ -27,34 +27,56 @@ const mockTodo = [
   },
 ]
 
+function reducer(state, action) {
+  // 상태 변화 코드
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) => it.id === action.targetId ? {...it, isDone: !it.isDone} : it);
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId)
+    }
+    default:
+      return state;
+  }
+}
+
+
 function App() {
 
   const idRef = useRef(3);
-  const [todo, setTodo] = useState(mockTodo);
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
 
   const onCreate = (content) => {
-    const newItem = {
-      id: 0,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    }
-    setTodo([newItem, ...todo]);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
+    
     idRef.current += 1;
   }
 
   // Todo Item을 수정하기 위한 함수
   const onUpdate = (targetId) => {
-    setTodo( 
-      // targetId를 가지는 Todo Item 상태를 변경하는 로직
-      todo.map(
-        (it) => it.id === targetId ? {...it, isDone: !it.isDone } : it
-      )
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    setTodo(todo.filter((it) => it.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   }
 
 
