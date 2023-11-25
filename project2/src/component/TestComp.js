de47dc06430c44f4005e8b966357b5ee703647c4
Joey <hsz3855@gmail.com>
2023-11-25 12:43:10 +0900

    add : useReducer 사용한 샘플 코드 추가

--- project2/src/component/TestComp.js
+++ project2/src/component/TestComp.js
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+
+    switch (action.type) {
+        case "INCREASE":
+            return state + action.data;
+        case "DECREASE":
+            return state - action.data;
+        default:
+            return state;
+        
+    }
+
+
+}
 
 function TestComp() {
 
-    const [count, setCount] = useState(0);
-
-    const onIncrease = () => {
-        setCount(count + 1);
-    }
-
-    const onDecrease = () => {
-        setCount(count - 1);
-    }
+    // count는 State를 의미한다.
+    // dispatch는 상태 변화 촉발 함수이다. State를 변경하고 싶으면 dispatch를 호출하면 된다.
+    // useReducer의 첫번째 인수는 State변화 함수, 두번째 인수는 State의 초기값
+    // 아래 문구를 정리하면 dispatch를 호출해서 상태 변경이 촉발된다면 reducer 함수가 실행된다는 뜻이다.
+    // reducer 함수의 반환값이 변경할 State의 값이 된다.
+    const [count, dispatch] = useReducer(reducer, 0);
 
     return (
         <div>
@@ -19,8 +31,8 @@ function TestComp() {
                 <bold>{count}</bold>
             </div>
             <div>
-               <button onClick={onIncrease}>+</button> 
-               <button onClick={onDecrease}>-</button> 
+               <button onClick={() => dispatch({ type: "INCREASE", data: 1})}>+</button> 
+               <button onClick={() => dispatch({ type: "DECREASE", data: 1})}>-</button> 
             </div>
         </div>
     )
