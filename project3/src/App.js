0567e57589122742abaa18cadd31e40e1dd8e361
Joey <hsz3855@gmail.com>
2023-11-28 00:07:50 +0900

    add : Route, Link 태그 사용 예시

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,14 +1,25 @@
-import { getEmotionImgById } from "./util";
-
+import { Routes, Route, Link} from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
 
 function App() {
   return (
     <div className="App">
-      <img alt="감정1" src={getEmotionImgById(1)}></img>
-      <img alt="감정2" src={getEmotionImgById(2)}></img>
-      <img alt="감정3" src={getEmotionImgById(3)}></img>
-      <img alt="감정4" src={getEmotionImgById(4)}></img>
-      <img alt="감정5" src={getEmotionImgById(5)}></img>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element={<New />}></Route>
+        <Route path="/diary" element={<Diary />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+      </Routes>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
+      </div>
     </div>
   );
 }
