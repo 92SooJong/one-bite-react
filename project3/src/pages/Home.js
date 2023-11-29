76b589be5dc96861cfc567ec55de29534b0c3e04
Joey <hsz3855@gmail.com>
2023-11-30 01:16:38 +0900

    add : 공통 스타일 설정

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -7,15 +7,7 @@ const Home = () => {
 
     return (
         <div>
-            <Editor 
-            initData={
-                {
-                    date: new Date().getTime(),
-                    emotionId: 1,
-                    content: "이전에 작성했던 일기",
-                }
-            }
-            onSubmit={() => {alert("작성 완료")}}></Editor>
+            Home 페이지입니다.
         </div>
     )
 };
