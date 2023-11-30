1723af809e92cb7427418f1af14a2e218fdd1a61
Joey <hsz3855@gmail.com>
2023-12-01 01:59:17 +0900

    add : 홈페이지 날짜 이동 적용

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -2,12 +2,29 @@ import { useSearchParams } from "react-router-dom";
 import Editor from "../component/\bEditor";
 import Button from "../component/Button";
 import Header from "../component/Header";
+import { useState } from "react";
 
 const Home = () => {
 
+    const [pivotDate, setPivotDate] = useState(new Date());
+
+    const onIncreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+    };
+
+    const onDecreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+    }
+
+    const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+
     return (
         <div>
-            Home 페이지입니다.
+            <Header
+                title={headerTitle}
+                leftChild={<Button text={"<"} onClick={onDecreaseMonth}></Button>}
+                rightChild={<Button text={">"} onClick={onIncreaseMonth}></Button>}>
+            </Header>
         </div>
     )
 };
