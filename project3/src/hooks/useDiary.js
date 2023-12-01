bc82a84709c318979f8495f3f4e46dc5415410ce
Joey <hsz3855@gmail.com>
2023-12-02 00:46:09 +0900

    add : 커스텀훅(useDiary) 뼈대 생성

---
+++ project3/src/hooks/useDiary.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { DiaryStateContext } from "../App";
+
+const useDiary = (id) => {
+
+    const data = useContext(DiaryStateContext);
+    return data;
+};
+export default useDiary;
