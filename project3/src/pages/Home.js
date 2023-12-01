92b7fc2a1866766169dc1cec92a14fd19db4b95d
Joey <hsz3855@gmail.com>
2023-12-01 16:09:57 +0900

    add : 날짜로 일기 필터링 하기

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -1,12 +1,24 @@
 import { useSearchParams } from "react-router-dom";
-import Editor from "../component/\bEditor";
 import Button from "../component/Button";
 import Header from "../component/Header";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import {DiaryStateContext} from "../App"
+import { getMonthRangeByDate } from "../util";
 
 const Home = () => {
 
+    const data = useContext(DiaryStateContext);
     const [pivotDate, setPivotDate] = useState(new Date());
+    const [filteredData, setFilteredData] = useState();
+
+    useEffect(() => {
+        if (data.length >= 1) {
+            const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+            setFilteredData(data.filter((it) => beginTimeStamp <= it.date && it.date <= endTimeStamp));
+        } else {
+            setFilteredData([]);
+        }
+    }, [data, pivotDate])
 
     const onIncreaseMonth = () => {
         setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
