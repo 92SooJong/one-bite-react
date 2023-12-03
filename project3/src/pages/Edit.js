81ecb086e0909507e91f0ddba62f1268ee641a98
Joey <hsz3855@gmail.com>
2023-12-04 00:10:43 +0900

    add : 일기 수정 기능 추가

--- project3/src/pages/Edit.js
+++ project3/src/pages/Edit.js
@@ -16,7 +16,7 @@ const Edit = () => {
     const goBack = () => {
         navigate(-1);
     }
-    const { onDelete } = useContext(DiaryDispatchContext);
+    const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
     const onClickDelete = () => {
         if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
             onDelete(id);
@@ -24,6 +24,13 @@ const Edit = () => {
         }
     }
 
+    const onSubmit = (data) => {
+        if (window.confirm("일기를 정말 수정할까요?")) {
+            const { date, content, emotionId } = data;
+            onUpdate(id, date, content, emotionId);
+            navigate("/", { replace: true });
+        }
+    }
 
     if (!data) {
         return <div>일기를 불러오고 있습니다</div>
@@ -35,7 +42,7 @@ const Edit = () => {
                     leftChild={<Button text={"< 뒤로 가기"} onClick={goBack}/>}
                     rightChild={<Button type={"negative"} text={"삭제하기"} onClick={onClickDelete}/>}
                 />
-                <Editor initData={data}></Editor>
+                <Editor initData={data} onSubmit={onSubmit}></Editor>
             </div>
         )
     }
